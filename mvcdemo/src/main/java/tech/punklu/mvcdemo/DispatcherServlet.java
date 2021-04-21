abdc418c38399180ef13fdacb7c48a81217c5f3b
punk1u <punk1u@protonmail.com>
2021-04-21 19:34:02 +0800

    支持ResponseBody及网页转发

--- mvcdemo/src/main/java/tech/punklu/mvcdemo/DispatcherServlet.java
+++ mvcdemo/src/main/java/tech/punklu/mvcdemo/DispatcherServlet.java
@@ -7,6 +7,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import tech.punklu.mvcdemo.annotation.Controller;
 import tech.punklu.mvcdemo.annotation.RequestMapping;
+import tech.punklu.mvcdemo.annotation.ResponseBody;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,7 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,14 +32,92 @@ public class DispatcherServlet extends HttpServlet {
 
 	private static Map<String,Method> methodMap = new HashMap<>();
 
+	private  static String prefix = "";
+	private  static String suffix = "";
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+ 		doPost(req,resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		/**
+		 * 获取到请求的URI
+		 */
+		String contextPath = req.getContextPath();
+		String requestURI = req.getRequestURI();
+		requestURI = requestURI.replace(contextPath,"");
+		Method method = methodMap.get(requestURI);
+		if (method != null){
+			/**
+			 * 获取方法的参数名称
+			 *
+			 * 这个使用方式需要：
+			 * javac -parameters进行编译
+			 * idea中找到File->Settings->java Compiler中的Additional command line parameters添加-parameters参数，然后clean install
+			 */
+			Parameter[] parameters = method.getParameters();
+			/**
+			 * 实参数组
+			 */
+			Object[] objects = new Object[parameters.length];
+ 			for (int i = 0; i < parameters.length; i++) {
+				Parameter parameter = parameters[i];
+				String name = parameter.getName();
+				Class type = parameter.getType();
+				if (type.equals(String.class)){
+					objects[i] = req.getParameter(name);
+				}else if(type.equals(HttpServletRequest.class)){
+					objects[i] = req;
+				}else if(type.equals(HttpServletResponse.class)){
+					objects[i] = resp;
+				}else {
+					/**
+					 * 处理对象参数，并给对象中的字段赋值
+					 */
+					try {
+						Object o = type.newInstance();
+						for(Field field : type.getDeclaredFields()){
+							field.setAccessible(true);
+							String fieldName = field.getName();
+							field.set(o,req.getParameter(fieldName));
+						}
+						objects[i] = o;
+					} catch (InstantiationException e) {
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			try {
+				Object o= null;
+				o = method.getDeclaringClass().newInstance();
+				Object invoke = method.invoke(o, objects);
+				// 判断返回值是否是Void
+				if (!method.getReturnType().equals(Void.class)){
+					ResponseBody annotation = method.getAnnotation(ResponseBody.class);
+					if (annotation!=null){
+						//提供接口来做这个事情
+						resp.getWriter().write(String.valueOf(invoke));
+					}else {
+						/**
+						 * 转发请求到指定页面
+						 */
+						// /page/index.html   page/index.html
+						req.getRequestDispatcher(prefix+String.valueOf(invoke)+suffix).forward(req,resp);
+					}
+
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			resp.setStatus(404);
+		}
+
 	}
 
 	@Override
@@ -46,6 +127,9 @@ public class DispatcherServlet extends HttpServlet {
 		File file = new File(projectPath + "//" + initParameter);
 		Document document = parse(file);
 		Element rootElement = document.getRootElement();
+		Element view = rootElement.element("view");
+		prefix = view.attribute("prefix").getValue();
+		suffix = view.attribute("suffix").getValue();
 		Element componentScan = rootElement.element(COMPONENT_SCAN_ELEMENT_NAME);
 		String packagePath = componentScan.attribute(COMPONENT_SCAN_ELEMENT_PACKAGE_NAME).getValue();
 		packagePath = packagePath.replace(".","\\");
--- mvcdemo/src/main/java/tech/punklu/mvcdemo/controller/TestController.java
+++ mvcdemo/src/main/java/tech/punklu/mvcdemo/controller/TestController.java
@@ -2,13 +2,25 @@ package tech.punklu.mvcdemo.controller;
 
 import tech.punklu.mvcdemo.annotation.Controller;
 import tech.punklu.mvcdemo.annotation.RequestMapping;
+import tech.punklu.mvcdemo.annotation.ResponseBody;
+import tech.punklu.mvcdemo.entity.UserEntity;
 
 @Controller
 @RequestMapping("/testController")
 public class TestController {
 
 	@RequestMapping("/testMapping.do")
-	public void test(){
+	@ResponseBody
+	public String test(String name, String age, UserEntity userEntity){
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(userEntity);
+		System.out.println("执行了");
+		return "testMapping.do";
+	}
 
+	@RequestMapping("/model.do")
+	public Object model(){
+ 		return "index";
 	}
 }
